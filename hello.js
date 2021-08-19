2313b5f0609fc0a549aa67292999825bbede47fe
Katie <katie.herda2@gmail.com>
2021-08-19 16:31:30 +0000

    Examples added to compare console.log vs return

--- hello.js
+++ hello.js
@@ -13,9 +13,25 @@ sayHello2("Katie");
 sayHello2("Artur");
 sayHello2("Debbie")
 
-
+//Practice
 const sayHello3 = function (name) {
   console.log('Heyyyaaa, ' + name)
 }
 
 sayHello3("Colin");
+
+//Return vs console.log
+//Below code immediately outputs 'Hello, John' to the console.
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+//Below code returns the string 'Hello, John' to the variable but nothing is output to the console. However, when a function returns a value, we can still console.log it later.
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John'); 
+console.log(greeting) //Calling function to be console.logged.
