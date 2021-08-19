bea04d575a715a4cb6e99048cd2458b419ad38ff
Katie <katie.herda2@gmail.com>
2021-08-19 16:13:24 +0000

    Use functions to say hello

---
+++ hello.js
@@ -0,0 +1,21 @@
+//Hello, world function
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+sayHello();
+
+//Function with a parameter, takes in a value as an input.
+const sayHello2 = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello2("Katie");
+sayHello2("Artur");
+sayHello2("Debbie")
+
+
+const sayHello3 = function (name) {
+  console.log('Heyyyaaa, ' + name)
+}
+
+sayHello3("Colin");
