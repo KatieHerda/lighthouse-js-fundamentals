1aaa2049cdb5ee567e9d4e71eec25854dae4c104
Katie <katie.herda2@gmail.com>
2021-08-20 22:14:55 +0000

    Use arrays and indexes to organize and retrieve ice-cream flaours

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Add root beer to the list of flavours
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Someone wants the first flavour.
+console.log(iceCreamFlavours[0]);
+
+//Somone wants the last flavour.
+console.log(iceCreamFlavours[5]);
+
+//Someone wants the last flavour.
+console.log(iceCreamFlavours.length)
